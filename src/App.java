dbd791c287fc5cd73e9c9c1683bbd6fbae0fa5a4
MyUserNameIsDat <100176345+MyUserNameIsDat@users.noreply.github.com>
2022-02-22 13:50:12 -0500

    Update App.java
    made it run the GUI method as the System Print is now useless

--- src/App.java
+++ src/App.java
@@ -3,10 +3,7 @@ public class App {
     public static boolean funny=false;
 
     public static void main(String[] args) throws Exception {
-
-        if (getAgreeal.ToS()) {
-            while (!App.funny) {
-             System.out.println("\n" + SystemInterface.result(SystemInterface.getJoke())+"\n");
+        new GUI(); // That starts the GUI Method
             }
         } 
     }
